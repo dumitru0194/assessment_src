b74418bfc9af8a00ccf3bbae3608f29134308d30
Pintilie Dumitru <78504524+dumitru0194@users.noreply.github.com>
2024-03-19 17:50:43 +0200

    Update mysql5.7-installation.sh

--- mysql5.7-installation.sh
+++ mysql5.7-installation.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-export DEBIAN_FRONTEND=noninteractive
-export MYSQL_REPO_URL="https://repo.mysql.com/apt/ubuntu"
-export MYSQL_SERVER_VERSION="mysql-5.7"
-export MYSQL_TOOLS="mysql-tools"
-export DISTRO="ubuntu"
-export DISTRO_VERSION="bionic"
-apt install wget
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
 dpkg -i mysql-apt-config_0.8.12-1_all.deb

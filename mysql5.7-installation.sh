18ca7a9ad77d823730c8d7ae3400d6e13bb34bc2
Pintilie Dumitru <78504524+dumitru0194@users.noreply.github.com>
2024-03-20 04:36:13 +0200

    Update mysql5.7-installation.sh

--- mysql5.7-installation.sh
+++ mysql5.7-installation.sh
@@ -17,5 +17,4 @@ DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.12-1_all.deb
 apt update
 debconf-set-selections <<< 'mysql-community-server mysql-community-server/root-pass password root'
 debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password root'
-yes | apt install -y -f 'mysql-client=5.7*' 'mysql-community-server=5.7*' 'mysql-server=5.7*'
-
+apt install -y --no-install-recommends 'mysql-client=5.7*' 'mysql-community-server=5.7*' 'mysql-server=5.7*'

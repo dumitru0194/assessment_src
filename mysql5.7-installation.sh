223bb80d8a2a7c0e2781291f70ddfb0527ef63cf
Pintilie Dumitru <78504524+dumitru0194@users.noreply.github.com>
2024-03-20 03:54:58 +0200

    Update mysql5.7-installation.sh

--- mysql5.7-installation.sh
+++ mysql5.7-installation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select bionic'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-preview select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
+
 apt-key --keyring /etc/apt/trusted.gpg.d/mysql-keyring.gpg adv --keyserver keyserver.ubuntu.com --recv B7B3B788A8D3785C
 dpkg -i mysql-apt-config_0.8.12-1_all.deb
 apt update
